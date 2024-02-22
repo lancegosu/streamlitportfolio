74d400dcef15b2a50efe5aa27e5d8405287dfdc2
Lance <106897295+lancegosu@users.noreply.github.com>
2024-02-22 02:25:24 -0800

    Update 3_Resume.py

--- pages/3_Resume.py
+++ pages/3_Resume.py
@@ -16,6 +16,5 @@ st.sidebar.markdown("[Github](https://github.com/lancegosu)")
 
 with open("images/resume.pdf", "rb") as f:
     base64_pdf = base64.b64encode(f.read()).decode("utf-8")
-    # pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="900" height="1150" type="application/pdf"></iframe>'
-    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="900" height="1150" type="application/pdf"></iframe>'
+    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="900" height="1150" type="application/pdf">'
     st.markdown(pdf_display, unsafe_allow_html=True)
