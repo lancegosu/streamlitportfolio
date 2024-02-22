e726146e4087acb78d9283fc048c9cadb7bc2ade
Lance <106897295+lancegosu@users.noreply.github.com>
2024-02-22 02:36:40 -0800

    Update 3_Resume.py

--- pages/3_Resume.py
+++ pages/3_Resume.py
@@ -18,12 +18,12 @@ with open("images/resume.pdf", "rb") as f:
     base64_pdf = base64.b64encode(f.read()).decode("utf-8")
     
     # Embedding PDF in HTML
-    pdf_display = f"""<embed
+    pdf_display = f"""<iframe
         class="pdfobject"
         type="application/pdf"
         title="Embedded PDF"
         src="data:application/pdf;base64,{base64_pdf}"
-        style="overflow: auto; width: 100%; height: 100%;">"""
+        style="overflow: auto; width: 100%; height: 100%;"></iframe>"""
 
     # Displaying File
     st.markdown(pdf_display, unsafe_allow_html=True)
