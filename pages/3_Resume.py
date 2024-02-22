f9ed5cd338ce1f005e4851fbda5ea4efe5a22f3a
Lance <106897295+lancegosu@users.noreply.github.com>
2024-02-22 02:35:24 -0800

    Update 3_Resume.py

--- pages/3_Resume.py
+++ pages/3_Resume.py
@@ -16,5 +16,14 @@ st.sidebar.markdown("[Github](https://github.com/lancegosu)")
 
 with open("images/resume.pdf", "rb") as f:
     base64_pdf = base64.b64encode(f.read()).decode("utf-8")
-    pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="900" height="1150" type="application/pdf">'
+    
+    # Embedding PDF in HTML
+    pdf_display = f"""<embed
+        class="pdfobject"
+        type="application/pdf"
+        title="Embedded PDF"
+        src="data:application/pdf;base64,{base64_pdf}"
+        style="overflow: auto; width: 100%; height: 100%;">"""
+
+    # Displaying File
     st.markdown(pdf_display, unsafe_allow_html=True)
