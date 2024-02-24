fb8edfda06a48572ed435f9ca10a641f70640ee5
Lance <106897295+lancegosu@users.noreply.github.com>
2024-02-24 12:59:20 -0800

    Update 2_Project_Overview.py

--- pages/2_Project_Overview.py
+++ pages/2_Project_Overview.py
@@ -24,46 +24,43 @@ st.sidebar.download_button(label="Download Resume",
 
 st.write("My goal in creating AI projects is to harness the transformative capabilities of Large Language Models (LLMs) and seamlessly integrate them into diverse applications. By leveraging the power of LLMs, I aim to enhance the intelligence, functionality, and user experience of these applications, pushing the boundaries of what AI can achieve in various domains. This endeavor reflects my commitment to unlocking the full potential of artificial intelligence to drive innovation and elevate the impact of technology in our daily lives.")
 
-st.write("[SpotOn](https://spoton.streamlit.app/) is a web application that integrates OpenAI's GPT-3.5-turbo language model with the Google Places API. This fusion results in a sophisticated platform for place search and intelligent question-answering.")
+st.write("[SpotOn](https://spoton.streamlit.app/) is a web application that integrates the OpenAI API with the Google Places API, resulting in a question-answering platform about place reviews.")
 with st.expander("**Key Features**:"):
     st.markdown("""
         - **Review-based Q&A**: Leverage place reviews to answer user queries using OpenAI's GPT-3.5-turbo language model.
         - **Place Search**: Utilize the Google Places API to search for places based on user queries.
         - **Interactive Map**: Display search results on an interactive map for enhanced user experience.
-        - **Dynamic UI**: Dynamically update the interface to allow users to ask questions about selected places and receive AI-generated responses.
         
         **APIs**: OpenAI ChatGPT API, Google Places API, Google Maps API
         
         **Libraries**: requests
         """)
 
-st.write("[Qdoc](https://qdocst.streamlit.app/) is a web application that utilizes a Large Language Model (LLM) to summarize articles from a given URL or PDF. Additionally, it can answer questions related to the article using the conversation history, the article's content, and common knowledge.")
+st.write("[Qdoc](https://qdocst.streamlit.app/) is a web application that utilizes a LLM to summarize articles and PDFs from a given URL. Additionally, it can answer questions related to the article using the conversation history, the article's content, and common knowledge.")
 with st.expander("**Key Features**:"):
     st.markdown("""
-    - **Summarization from URL or PDF**: Summarize the contents of an article by providing a URL or PDF link.
+    - **Summarization from URL**: Summarize the contents of an article or PDF by providing a URL.
     - **Question-Answering**: Inquire about specific details or seek clarification regarding the article, and the system generates answers based on conversation history and article content.
     - **Conversation History**: Ask follow-up questions based on the ongoing conversation.
-    - **Refresh Conversation**: Clear the conversation history to initiate a fresh dialogue.
     
     **APIs**: OpenAI ChatGPT API
     
     **Libraries**: OpenAI, requests, BeautifulSoup, PyMuPDF
     """)
 
-st.write("[CooPA](https://coopas.streamlit.app/) is a web application that leverages OpenAI's ChatGPT API and the Google Custom Search API to deliver contextually informed answers by aggregating relevant content from online articles based on user queries.")
+st.write("[CooPA](https://coopas.streamlit.app/) is a chatbot that uses the OpenAI API to deliver contextually informed answers by aggregating relevant content from Google search based on user queries.")
 with st.expander("**Key Features**:"):
     st.markdown("""
-    - **Smart Search**: Perform intelligent searches using Google Custom Search and aggregate content from relevant articles to generate comprehensive answers.
+    - **Smart Search**: Aggregates content from relevant articles on Google search.
     - **Language Model Interaction**: Utilize OpenAI's GPT-3.5-turbo for generating contextually informed responses to user queries.
-    - **Speech Recognition and Synthesis**: Enable users to input queries through speech and listen to the synthesized responses.
-    - **Source Attribution**: Automatically include source URLs when presenting the aggregated content, offering transparency and credibility.
+    - **Citation**: Automatically include source URLs when presenting the aggregated content, offering transparency and credibility.
     
     **APIs**: OpenAI ChatGPT API, Google Custom Search API
     
     **Libraries**: OpenAI, requests, BeautifulSoup
     """)
 
-st.write("[aiXtensions](https://github.com/lancegosu/aixtensions) consists of two independent Chrome extensions: LookItUp and Persona, each leveraging OpenAI's ChatGPT API for text-related tasks.")
+st.write("[aiXtensions](https://github.com/lancegosu/aixtensions) consists of two independent Chrome extensions: LookItUp and Persona, each leveraging OpenAI API for text-related tasks.")
 with st.expander("**Key Features**:"):
     st.markdown("""
     - **LookItUp**: Allows users to define highlighted text on a webpage using OpenAI's GPT-3.5-turbo.
