52f01c387474879ee17b31443e5306bb79f2efc1
Lance <106897295+lancegosu@users.noreply.github.com>
2024-02-24 20:16:29 -0800

    Update 1_About_Me.py

--- pages/1_About_Me.py
+++ pages/1_About_Me.py
@@ -22,26 +22,31 @@ st.sidebar.download_button(label="Download Resume",
                            file_name="Lance-Nguyen-Resume.pdf",
                            mime='application/octet-stream')
 
-st.header("Where I'm From")
-st.write("""
-I was born and raised in California, a place blessed with some of the most beautiful weather on the planet. 
-Growing up in the Golden State fueled my passion for exploration, be it the bustling city life or the tranquility of 
-the redwood forests. This unique blend of experiences has not only shaped my appreciation for diversity but also 
-influences the creativity I bring to my work as a developer. From the Pacific shores to the tech hubs, California is 
-more than my home; it's a constant source of inspiration. 🌞🌴
-""")
+left, right = st.columns(2)
+with left:
+    st.header("Where I'm From")
+    st.write("""
+    I was born and raised in California, a place blessed with some of the most beautiful weather on the planet. 
+    Growing up in the Golden State fueled my passion for exploration, be it the bustling city life or the tranquility of 
+    the redwood forests. This unique blend of experiences has not only shaped my appreciation for diversity but also 
+    influences the creativity I bring to my work as a developer. From the Pacific shores to the tech hubs, California is 
+    more than my home; it's a constant source of inspiration. 🌞🌴
+    """)
+    
+    st.header("What I Do Now")
+    st.write("""
+    Currently, I'm immersed in the world of software development, transforming ideas into vibrant tech solutions. 
+    Whether it's crafting sleek websites, optimizing code for AI magic, or diving into the latest tech trends, 
+    I'm all about making the digital realm exciting and user-friendly. Let's build something extraordinary together!
+    """)
+    
+    st.header("What I Used to Do")
+    st.write("""
+    In my past life, I spent over 8 years as a professional tennis coach, shaping the game and personal development of 
+    more than 600 students. From empowering beginners to refining strategies for national-level players, each lesson was a 
+    unique journey. Transitioning into software engineering, I carry the adaptability and personalized touch from the 
+    tennis court into the world of coding, creating tech solutions with a human touch. 🎾💻
+    """)
 
-st.header("What I Do Now")
-st.write("""
-Currently, I'm immersed in the world of software development, transforming ideas into vibrant tech solutions. 
-Whether it's crafting sleek websites, optimizing code for AI magic, or diving into the latest tech trends, 
-I'm all about making the digital realm exciting and user-friendly. Let's build something extraordinary together!
-""")
-
-st.header("What I Used to Do")
-st.write("""
-In my past life, I spent over 8 years as a professional tennis coach, shaping the game and personal development of 
-more than 600 students. From empowering beginners to refining strategies for national-level players, each lesson was a 
-unique journey. Transitioning into software engineering, I carry the adaptability and personalized touch from the 
-tennis court into the world of coding, creating tech solutions with a human touch. 🎾💻
-""")
+with right:
+    st.image("images/me.jpg")
