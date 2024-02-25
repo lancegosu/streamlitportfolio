31321b806f5a4cc6a9d8f2d80240c4d31a26ff05
Lance <106897295+lancegosu@users.noreply.github.com>
2024-02-25 05:13:26 -0800

    Update 2_Project_Overview.py

--- pages/2_Project_Overview.py
+++ pages/2_Project_Overview.py
@@ -54,7 +54,7 @@ with st.expander("**Key Features**:"):
 
 st.write("""
 [Qdoc](https://qdocst.streamlit.app/) is a web application that utilizes a LLM to summarize articles and PDFs from a 
-given URL. Additionally, it can answer questions related to the article using the conversation history, 
+given URL. Additionally, it can answer questions regarding the article using the history of the conversation, 
 the article's content, and common knowledge.
 """)
 with st.expander("**Key Features**:"):
