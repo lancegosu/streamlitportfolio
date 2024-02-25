568f84219eb71bf08a8856d49b4019e829e9add3
Lance <106897295+lancegosu@users.noreply.github.com>
2024-02-24 16:52:46 -0800

    Update 3_Resources.py

--- pages/3_Resources.py
+++ pages/3_Resources.py
@@ -23,16 +23,36 @@ st.sidebar.download_button(label="Download Resume",
                            mime='application/octet-stream')
 
 st.markdown("[Harvard Online: CS50P - Introduction to Programming with Python](https://pll.harvard.edu/course/cs50s-introduction-programming-python)")
-st.write("David Malan offers an accessible entry into the world of programming by teaching Python, a versatile and widely-used programming language known for its readability and flexibility. Python's prominence in artificial intelligence and machine learning applications adds an extra layer of significance to the course, making it an ideal starting point for those aspiring to go into the exciting realms of AI/ML.")
+st.write("""
+David Malan offers an accessible entry into the world of programming by teaching Python, 
+a versatile and widely-used programming language known for its readability and flexibility. 
+Python's prominence in artificial intelligence and machine learning applications adds an extra layer of significance to 
+the course, making it an ideal starting point for those aspiring to go into the exciting realms of AI/ML.
+""")
 
 st.markdown("[fast.ai - Practical Deep Learning](https://course.fast.ai/)")
-st.write("Created by Jeremy Howard and Rachel Thomas, the fast.ai course begins with offering a more hands-on approach by using AI tools in real-world examples before explaining how the tools work.")
+st.write("""
+Created by Jeremy Howard and Rachel Thomas, the fast.ai course begins with offering a more hands-on approach 
+by using AI tools in real-world examples before explaining how the tools work.
+""")
 
 st.markdown("[Andrej Karpathy's Neural Networks: Zero to Hero on YouTube](https://www.youtube.com/playlist?list=PLAqhIrjkxbuWI23v9cThsA9GvCAUhRvKZ)")
-st.write("Andrej Karpathy's series of lectures describe the evolution of language modeling and how to build different architectural models from scratch to give a better understanding of the intricacies of modern language models.")
+st.write("""
+Andrej Karpathy's series of lectures describe the evolution of language modeling and how to build different 
+architectural models from scratch to give a better understanding of the intricacies of modern language models.
+""")
 
 st.markdown("[DeepLearning.AI Short Courses](https://www.deeplearning.ai/short-courses/)")
-st.write("DeepLearning.AI covers various aspects of generative AI and is an excellent resource for both beginners and those looking to deepen their knowledge in the field.")
+st.write("""
+DeepLearning.AI covers various aspects of generative AI and is an excellent resource for both beginners and 
+those looking to deepen their knowledge in the field.
+""")
 
 st.markdown("[Presentation: Fine-Tuning OpenAI’s GPT 3.5 to Unlock Enterprise Use Cases](https://exchange.scale.com/public/events/fine-tuning-open-ais-advanced-base-model-gpt-3-5-2023-11-08)")
-st.write("Speakers Colin Jarvis and Luv Kothari emphasize the power of optimizing and fine-tuning LLMs for critical use cases to meet industry demands. They provide insights into the strategic balance (and difference) between retrieval augmented generation for content enrichment, and fine-tuning to enhance the model's capabilities. The presentation serves as a valuable guide for enterprises to ensure proficiency and confidence in harnessing transformative LLM capabilities.")
+st.write("""
+Speakers Colin Jarvis and Luv Kothari emphasize the power of optimizing and fine-tuning LLMs for critical use cases to 
+meet industry demands. They provide insights into the strategic balance (and difference) between 
+retrieval augmented generation for content enrichment, and fine-tuning to enhance the model's capabilities. 
+The presentation serves as a valuable guide for enterprises to ensure proficiency and confidence in harnessing 
+transformative LLM capabilities.
+""")
